9a2ce848f0b8bf529d779e6fdc6266da22a1a561
unknown <dev.prapya@gmail.com>
2024-10-12 11:20:20 +0600

    user routes optimized

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -4,10 +4,13 @@ const userValidations = require("./users.validation")
 const userControllers = require("./users.controller");
 const varifyToken = require("../../middlewares/varifyToken");
 
+// public routes
 userRouter.post("/", validateRequest(userValidations.creatUserValidationSchema), userControllers.createUser);
 userRouter.post("/login", validateRequest(userValidations.userLoginValidationSchema), userControllers.loginUser)
+// admin approval needed routes
 userRouter.get("/", userControllers.getAllUsers);
 userRouter.get("/:id", userControllers.getSingleUser);
+// user must be logged in route
 userRouter.patch("/", validateRequest(userValidations.updateUserValidationSchema), varifyToken, userControllers.updateUser);
 
 module.exports = userRouter;
