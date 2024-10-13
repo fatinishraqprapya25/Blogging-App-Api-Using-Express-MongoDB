fc32da47be891aa763581c0ab9e6bf440cf8eb3b
unknown <dev.prapya@gmail.com>
2024-10-13 16:25:44 +0600

    admin model done

---
+++ modules/admin/admin.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    addedBy: {
+        type: mongoose.Types.ObjectId,
+        ref: "Admin"
+    }
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
