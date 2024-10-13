124475049e9d9ee2d3ac96a230ad621462d889c1
unknown <dev.prapya@gmail.com>
2024-10-13 21:00:18 +0600

    some minor problems fixed

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -6,13 +6,14 @@ const varifyToken = require("../../middlewares/varifyToken");
 
 const upload = require("../../utils/upload");
 const userUtils = require("./users.utils");
+const varifyAdmin = require("../../middlewares/varifyAdmin");
 
 // public routes
 userRouter.post("/", upload.single("file"), validateRequest(userValidations.creatUserValidationSchema, userUtils.deteteUploadedPhotoIfValidationFailed), userControllers.createUser);
 userRouter.post("/login", validateRequest(userValidations.userLoginValidationSchema), userControllers.loginUser)
 // admin approval needed routes
-userRouter.get("/", userControllers.getAllUsers);
-userRouter.get("/:id", userControllers.getSingleUser);
+userRouter.get("/", varifyAdmin, userControllers.getAllUsers);
+userRouter.get("/:id", varifyAdmin, userControllers.getSingleUser);
 // user must be logged in route
 userRouter.patch("/", validateRequest(userValidations.updateUserValidationSchema), varifyToken, userControllers.updateUser);
 
