9a52ed5eba87b6405f62facd0a5c43ebb6f52613
unknown <dev.prapya@gmail.com>
2024-10-14 15:34:22 +0600

    deleting blog image if validation failed done

--- modules/blogs/blogs.route.js
+++ modules/blogs/blogs.route.js
@@ -4,7 +4,8 @@ const blogControllers = require("./blog.controller");
 const upload = require("../../utils/upload");
 const validateRequest = require("../../middlewares/validateRequest");
 const blogValidations = require("./blogs.validation");
+const blogUtils = require("./blogs.utils")
 
-blogRouter.post("/", varifyToken, upload("blogs").single("file"), validateRequest(blogValidations.blogCreationValidationSchema), blogControllers.createBlog);
+blogRouter.post("/", varifyToken, upload("blogs").single("file"), validateRequest(blogValidations.blogCreationValidationSchema, blogUtils.deteteUploadedPhotoIfValidationFailed), blogControllers.createBlog);
 
 module.exports = blogRouter;
--- modules/blogs/blogs.utils.js
+++ modules/blogs/blogs.utils.js
@@ -1,16 +1,15 @@
 const path = require("path");
 const deleteUploadedFile = require("../../errors/deleteUploadedFile");
 
-const userUtils = {};
+const blogUtils = {};
 
-userUtils.deteteUploadedPhotoIfValidationFailed = function (msg) {
+blogUtils.deteteUploadedPhotoIfValidationFailed = function (msg) {
     if (!msg.success) {
         if (msg.req.file) {
             const filePath = path.join(__dirname, "../../", msg.req.file.path);
-            console.log(__dirname + "\n" + msg.req.file)
             deleteUploadedFile(filePath);
         }
     }
 }
 
-module.exports = userUtils;
+module.exports = blogUtils;
