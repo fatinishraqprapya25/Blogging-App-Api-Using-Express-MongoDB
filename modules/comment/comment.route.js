a25e617a120129d8aacb5646f45df5ef7093fe1f
unknown <dev.prapya@gmail.com>
2024-10-16 16:41:40 +0600

    validations for creating or updating comment done

--- modules/comment/comment.route.js
+++ modules/comment/comment.route.js
@@ -1,11 +1,14 @@
 const commentControllers = require("./comment.controller");
 const varifyToken = require("../../middlewares/varifyToken");
 const commentUtils = require("./comments.utils");
+const validateRequest = require("../../middlewares/validateRequest");
+const commentValidations = require("./comment.validation");
+
 const commentRouter = require("express").Router();
 
-commentRouter.post("", varifyToken, commentControllers.createComment);
+commentRouter.post("", varifyToken, validateRequest(commentValidations.createComment), commentControllers.createComment);
 commentRouter.get("/:postId", varifyToken, commentControllers.readComments);
 commentRouter.delete("/:commentId", varifyToken, commentUtils.checkAdminOrAuthorForDelete, commentControllers.deleteComment);
-commentRouter.patch("/:commentId", varifyToken, commentUtils.checkCommentAuthorForUpdate, commentControllers.updateComment);
+commentRouter.patch("/:commentId", varifyToken, commentUtils.checkCommentAuthorForUpdate, validateRequest(commentValidations.updateComment), commentControllers.updateComment);
 
 module.exports = commentRouter;
---
+++ modules/comment/comment.validation.js
@@ -0,0 +1,21 @@
+const { z } = require("zod");
+
+const createComment = z.object({
+    body: z.object({
+        blogId: z.string().min(24, "Invalid Blog ID").max(24, "Invalid Blog ID"),
+        userId: z.string().min(24, "Invalid User ID").max(24, "Invalid User ID"),
+        text: z.string().min(1, "Comment text is required").trim(),
+    })
+});
+
+const updateComment = z.object({
+    body: z.object({
+        blogId: z.string().min(24, "Invalid Blog ID").max(24, "Invalid Blog ID").optional(),
+        userId: z.string().min(24, "Invalid User ID").max(24, "Invalid User ID").optional(),
+        text: z.string().min(1, "Comment text is required").trim().optional(),
+    })
+});
+
+const commentValidations = { createComment, updateComment };
+
+module.exports = commentValidations;
