a6bba77b735e734c0e2f1f054c0f0e02c33404c5
unknown <dev.prapya@gmail.com>
2024-10-19 12:11:40 +0600

    helmet js intregated

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = require("./routes");
+const helmet = require("helmet");
 const globalErrorHandler = require("./errors/globalErrorHandler");
 const notFoundHandler = require("./errors/notFoundHandler");
 
 const app = express();
-app.use(express.json());  // json perser
-app.use("/api/v1", router) // router
-app.use(globalErrorHandler) // global error handler
-app.use(notFoundHandler) // not found handler
+
+app.use(helmet());
+app.use(express.json());
+app.use("/api/v1", router)
+app.use(globalErrorHandler)
+app.use(notFoundHandler)
 
 module.exports = app;
