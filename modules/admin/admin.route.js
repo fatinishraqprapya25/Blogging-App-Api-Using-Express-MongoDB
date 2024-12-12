ce9e25962a20e1453d743e1f5c920eab5ca9f138
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2024-12-12 20:16:20 +0600

    basic router for blog approve by admin done

--- modules/admin/admin.route.js
+++ modules/admin/admin.route.js
@@ -2,10 +2,14 @@ const varifyAdmin = require("../../middlewares/varifyAdmin");
 const adminControllers = require("./admin.controller");
 const adminRouter = require("express").Router();
 const validateRequest = require("../../middlewares/validateRequest");
-const adminCreationValidationSchema = require("./admin.validation");
+const { adminCreationValidationSchema, adminBlogApproveValidationSchema } = require("./admin.validation");
 
+// admin features
 adminRouter.post("/", varifyAdmin, validateRequest(adminCreationValidationSchema), adminControllers.createAdmin);
 adminRouter.delete("/:id", varifyAdmin, adminControllers.removeAdmin);
 adminRouter.get("/", varifyAdmin, adminControllers.getAllAdmin);
+
+// blog features
+adminRouter.post("/", varifyAdmin, validateRequest(adminBlogApproveValidationSchema), adminControllers.approveBlog);
 
 module.exports = adminRouter;
--- modules/admin/admin.validation.js
+++ modules/admin/admin.validation.js
@@ -6,4 +6,10 @@ const adminCreationValidationSchema = z.object({
     })
 });
 
-module.exports = adminCreationValidationSchema;
+const adminBlogApproveValidationSchema = z.object({
+    body: z.object({
+        user: z.string().length(24, "User ID must be 24 characters long").regex(/^[0-9a-fA-F]{24}$/, "User ID must be a valid ObjectId")
+    })
+});
+
+module.exports = { adminCreationValidationSchema, adminBlogApproveValidationSchema };
