2072cf765b19677e1c9a46db3461cc3602d158ca
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2024-12-14 11:20:40 +0600

    track Traffic middleware done

---
+++ middlewares/trackTraffic.js
@@ -0,0 +1,18 @@
+const trackTraffic = (req, res, next) => {
+    const userId = req.user ? req.user : null;
+    const userType = req.user ? "authenticated" : "general";
+    const timestamp = new Date();
+
+    const trafficData = {
+        userId,
+        userType,
+        route: req.originalUrl,
+        method: req.method,
+        timestamp
+    };
+
+    console.log(trafficData);
+    saveTrafficData(trafficData);
+    next();
+
+}
