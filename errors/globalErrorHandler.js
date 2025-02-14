d3d0bc58c8471cd0f04f9d8ddebd8c3702504589
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-14 15:30:29 +0600

    global Error handler optimimzed

--- errors/globalErrorHandler.js
+++ errors/globalErrorHandler.js
@@ -1,9 +1,32 @@
+const { ZodError } = require("zod");
+const multer = require("multer");
+const sendResponse = require("../utils/sendResponse");
+
 const globalErrorHandler = (error, req, res, next) => {
-    res.json({
-        status: 500,
-        message: error.message,
-        error
-    });
-}
+    let statusCode = error.statusCode || 500;
+    let message = error.message || "Internal Server Error";
+    let errorDetails = {};
+
+    if (error instanceof ZodError) {
+        statusCode = 400;
+        message = "Validation failed";
+        errorDetails = error.errors.map(err => ({
+            path: err.path.join("."),
+            message: err.message
+        }));
+    }
+
+    else if (error instanceof multer.MulterError) {
+        statusCode = 400;
+        message = "File upload error";
+        errorDetails = { field: error.field, message: error.message };
+    }
+
+    sendResponse(res, statusCode, {
+        success: false,
+        message,
+        error: errorDetails
+    })
+};
 
 module.exports = globalErrorHandler;
