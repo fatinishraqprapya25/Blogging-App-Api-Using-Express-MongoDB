6885d3cca19e7d79fdff007a4bacaf0f6d5036a8
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-14 15:39:42 +0600

    verify code of userUtils done

--- modules/users/users.utils.js
+++ modules/users/users.utils.js
@@ -1,5 +1,7 @@
 const path = require("path");
+const jwt = require("jsonwebtoken");
 const deleteUploadedFile = require("../../errors/deleteUploadedFile");
+const User = require("./users.model");
 
 const userUtils = {};
 
@@ -10,6 +12,19 @@ userUtils.deteteUploadedPhotoIfValidationFailed = function (msg) {
             console.log(__dirname + "\n" + msg.req.file)
             deleteUploadedFile(filePath);
         }
+    }
+}
+
+userUtils.verifyCode = async (email, code) => {
+    try {
+        const user = await User.findOne({ email });
+        if (!user) throw new Error("user not found!");
+        const decoded = jwt.verify(user.verificationToken);
+        if (!decoded) throw new Error("invalid token");
+        if (parseInt(decode.verificationCode) === parseInt(code)) return true;
+        return false;
+    } catch (err) {
+        throw new Error(err.message);
     }
 }
 
