69bba81aafefa47267a694b7cb8af6d9dea1415a
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-02-14 17:00:09 +0600

    validations for send verification code & reseting password done

--- modules/users/users.validation.js
+++ modules/users/users.validation.js
@@ -10,6 +10,27 @@ const creatUserValidationSchema = z.object({
     })
 });
 
+const verifyUserValidation = z.object({
+    body: z.object({
+        email: z.string().email({ message: "Invalid Email Address" }),
+        code: string().min(6, { message: "Code must be at least 6 digits" })
+    })
+});
+
+const sendVerificationCodeValidation = z.object({
+    body: z.object({
+        email: z.string().email({ message: "Invalid Email Address" }),
+    })
+});
+
+const resetPasswordValidation = z.object({
+    body: z.object({
+        email: z.string().email({ message: "Invalid Email Address" }),
+        code: string().min(6, { message: "Code must be at least 6 digits" }),
+        password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+    })
+});
+
 const updateUserValidationSchema = z.object({
     body: z.object({
         firstName: z.string().min(1, { message: "First name must be at least 1 character long" }).optional(),
@@ -27,6 +48,6 @@ const userLoginValidationSchema = z.object({
     })
 });
 
-const userValidations = { creatUserValidationSchema, updateUserValidationSchema, userLoginValidationSchema };
+const userValidations = { creatUserValidationSchema, updateUserValidationSchema, userLoginValidationSchema, verifyUserValidation, resetPasswordValidation, sendVerificationCodeValidation };
 
 module.exports = userValidations
