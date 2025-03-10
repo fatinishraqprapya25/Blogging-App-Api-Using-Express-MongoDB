a23208aa44cd5a94b9e805fd056ce9a41962d855
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-03-10 23:22:42 +0600

    logged in users token time optimized

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("../config");
 
 const generateToken = (user) => {
-    return jwt.sign({ id: user._id, email: user.email }, config.jwtSecret, { expiresIn: "1h" });
+    return jwt.sign({ id: user._id, email: user.email }, config.jwtSecret, { expiresIn: "720h" });
 }
 
 module.exports = generateToken;
